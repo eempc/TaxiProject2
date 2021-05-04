1103d4b563c49a7b1605476cd296f0288ea141ae
Evelyn Cheng <evc776@gmail.com>
2021-05-04 20:45:13 +0100

    seeded project files of the later implementation

---
+++ src/com/company/Actor.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * An actor in the taxi-company simulation.
+ *
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29
+ */
+public interface Actor {
+    /**
+     * Implement the actor's behavior.
+     */
+    public void act();
+}
